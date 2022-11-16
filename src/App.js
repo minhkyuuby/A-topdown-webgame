8db4656c25b8abd9720012b1a2371ab3444c6adb
Đoàn Văn Minh <minhkyuuby@gmail.com>
2022-11-16 11:07:09 +0700

    add Unity

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
+  const { unityProvider, loadingProgression } = useUnityContext({
+    loaderUrl: "Unity/Build/newtest.loader.js",
+    dataUrl: "Unity/Build/newtest.data",
+    frameworkUrl: "Unity/Build/newtest.framework.js",
+    codeUrl: "Unity/Build/newtest.wasm",
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
+      <Unity unityProvider={unityProvider} style={{ width: "80%", margin: "auto"}}/>
+    </React.Fragment>
   );
 }
 
